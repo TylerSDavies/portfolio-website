c7a34dd45601dc3fe7d84ca1e6253a7f4bc43e3a
KoDraws <113064614+TylerSDavies@users.noreply.github.com>
2024-07-20 11:43:07 -0700

    mobile fixes

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,6 +1,6 @@
 export default function PageContainer(props) {
     return (
-        <div className="flex flex-col w-full h-full p-8 md:p-16 md:justify-center overflow-auto  bg-gray-900">
+        <div className="flex flex-col w-full h-full p-8 pb-32 md:p-16 md:justify-center overflow-auto bg-gray-900">
             {props.children}
         </div>
     )
