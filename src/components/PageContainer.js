f8a24695861e03d2e5beeb5ba6fd314e20b95a42
KoDraws <113064614+TylerSDavies@users.noreply.github.com>
2024-07-21 09:35:28 -0700

    Update PageContainer.js

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,6 +1,9 @@
 export default function PageContainer(props) {
     return (
-        <div className="flex flex-col w-full h-full p-8 pb-32 md:p-16 md:justify-center overflow-auto bg-gray-900">
+        <div
+            className="flex flex-col w-full h-full p-8 pb-32 md:p-16 md:justify-center overflow-auto bg-gray-900"
+            style={{userSelect: 'text'}}
+        >
             {props.children}
         </div>
     )
