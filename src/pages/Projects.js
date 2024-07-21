9ef7a75ea41c23a4abf875e925eae215d5bf64fd
KoDraws <113064614+TylerSDavies@users.noreply.github.com>
2024-07-21 09:31:18 -0700

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -25,7 +25,7 @@ export default function Projects() {
                 </div>
                 <div className='flex flex-col min-w-72 flex-1'>
                     <h3 className='text-center text-sky-400'>fuX</h3>
-                    <p className='text-center'>NextJS-Based Web Application</p>
+                    <p className='text-center'>Chrome Extension</p>
                     <hr className='my-2' />
                     <div className='flex gap-4 w-full justify-center'>
                         <a className='text-center underline text-sky-400 hover:text-sky-600' href='https://github.com/TylerSDavies/fuX' target='_blank'>GitHub</a>
